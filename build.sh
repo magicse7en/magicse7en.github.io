79baa8051e61021b09a1d993524e27f9a3f38ccb
magicse7en <magicse7en@outlook.com>
2016-03-27 22:50:20 +0800

    hexo deploy

--- build.sh
+++
@@ -1,8 +0,0 @@
-hexo clean
-hexo g -d
-cd ./public #生成的静态页面会存储在public目录下
-git init
-git add --all .
-git commit -m "Travis CI Auto Builder" #自动构建后的内容将全部以此信息提交
-git push --quiet --force https://$DEPLOY_REPO@github.com/magicse7en/magicse7en.github.io.git master  #自动构建后的内容将全部以此信息提交
-#curl --connect-timeout 20 --max-time 30 -s http://远端服务器URL/webhook.php?_=${NOTIFY_TOKEN} #服务器Webhook 将在下文介绍
