49a7e98d2eeb84d3c22fa7e40ee05a7011db2748
magicse7en <magicse7en@outlook.com>
2016-03-27 22:35:03 +0800

    hexo deploy

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 hexo clean
 hexo g -d
 cd ./public #生成的静态页面会存储在public目录下
-#git init
+git init
 git add --all .
 git commit -m "Travis CI Auto Builder" #自动构建后的内容将全部以此信息提交
 git push --quiet --force https://$DEPLOY_REPO@github.com/magicse7en/magicse7en.github.io.git master  #自动构建后的内容将全部以此信息提交
